314323bced1c32d12c016c837b2f4869acc7e6a8
Kevin Carlos de Lucena <kevinlucena595@hotmail.com>
2025-05-01 14:53:52 -0300

    resolvido conflito de merge

--- app.py
+++ app.py
@@ -53,8 +53,12 @@ def create_answers_section(df):
 
 def main():
     df_raw = load_data()
+
     create_dataframe_section(df_raw)
+
     create_answers_section(df_raw)
+
+    return None
 
 if __name__ == '__main__':
     main()
